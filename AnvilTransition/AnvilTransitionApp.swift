389bfd1b44671ab6274644966a6a8b87ad43510b
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-08-24 10:12:35 +0800

    Initial Commit

---
+++ AnvilTransition/AnvilTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnvilTransitionApp.swift
+//  AnvilTransition
+//
+//  Created by Jia Chen Yee on 24/8/22.
+//
+
+import SwiftUI
+
+@main
+struct AnvilTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
